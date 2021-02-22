2aca4419f68d58f38c4010fcc8807bd51be14c75
Mati <586617@stud.hvl.no>
2021-02-22 18:29:52 +0100

    regnr

--- src/no/hvl/dat109/objekt/Bilpark.java
+++ src/no/hvl/dat109/objekt/Bilpark.java
@@ -16,9 +16,9 @@ public class Bilpark {
 		
 		List<Bil> biler =new ArrayList<Bil>();
 	
-		Bil b1 = new Bil("", "Mercedes", "CLA250e", "Hvit", UtleieEnum.STOR_BIL, "2000");
+		Bil b1 = new Bil("BG1003", "Mercedes", "CLA250e", "Hvit", UtleieEnum.STOR_BIL, "700");
 		biler.add(b1);
-		Bil b2 = new Bil("", "Toyota", "Yaris", "Rød", UtleieEnum.MELLOMSTOR_BIL, "2000");
+		Bil b2 = new Bil("HO1002", "Toyota", "Yaris", "Rød", UtleieEnum.MELLOMSTOR_BIL, "600");
 		biler.add(b2);
 		
 		return biler;	
@@ -28,11 +28,11 @@ public class Bilpark {
 		
 		List<Bil> biler =new ArrayList<Bil>();
 		
-		Bil b1 = new Bil("", "VW", "Caddy", "Hvit", UtleieEnum.STASJONSVOGN, "2000");
+		Bil b1 = new Bil("KG4959", "VW", "Caddy", "Hvit", UtleieEnum.STASJONSVOGN, "800");
 		biler.add(b1);
-		Bil b2 = new Bil("", "KIA", "Sport", "Grå", UtleieEnum.MELLOMSTOR_BIL, "2000");
+		Bil b2 = new Bil("FK2030", "KIA", "Sport", "Grå", UtleieEnum.MELLOMSTOR_BIL, "600");
 		biler.add(b2);
-		Bil b3 = new Bil("", "Opel", "Ampera", "Rød", UtleieEnum.LITEN_BIL, "2000");
+		Bil b3 = new Bil("SK3993", "Opel", "Ampera", "Rød", UtleieEnum.LITEN_BIL, "500");
 		biler.add(b3);
 		
 		return biler;
@@ -42,15 +42,15 @@ public class Bilpark {
 		
 		List<Bil> biler =new ArrayList<Bil>();
 		
-		Bil b1 = new Bil("", "Jaguar", "I-Pace", "Hvit", UtleieEnum.LITEN_BIL, "2000");
+		Bil b1 = new Bil("SO2030", "Jaguar", "I-Pace", "Hvit", UtleieEnum.LITEN_BIL, "500");
 		biler.add(b1);
-		Bil b2 = new Bil("", "Nissan", "Leaf", "Grå", UtleieEnum.MELLOMSTOR_BIL, "2000");
+		Bil b2 = new Bil("FG1304", "Nissan", "Leaf", "Grå", UtleieEnum.MELLOMSTOR_BIL, "600");
 		biler.add(b2);
-		Bil b3 = new Bil("", "Toyota", "Yaris", "Blå", UtleieEnum.STOR_BIL, "2000");
+		Bil b3 = new Bil("TO0000", "Toyota", "Yaris", "Blå", UtleieEnum.STOR_BIL, "700");
 		biler.add(b3);
-		Bil b4 = new Bil("", "Volvo", "V90", "Grå", UtleieEnum.STASJONSVOGN, "2000");
+		Bil b4 = new Bil("IO5486", "Volvo", "V90", "Grå", UtleieEnum.STASJONSVOGN, "800");
 		biler.add(b4);
-		Bil b5 = new Bil("", "", "", "", UtleieEnum.MELLOMSTOR_BIL, "2000");
+		Bil b5 = new Bil("WU5869", "VW", "Touran", "Svart", UtleieEnum.MELLOMSTOR_BIL, "600");
 		biler.add(b5);
 		
 		return biler;
